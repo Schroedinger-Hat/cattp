b6a4b34d75e380fef946638cea2bbb28cf45cae3
404answernotfound <404answernotfound.public@gmail.com>
2023-06-19 23:25:17 +0200

    Homebrew install

--- Formula/cattp.rb
+++ Formula/cattp.rb
@@ -2,7 +2,7 @@ class Loremclipsum < Formula
     desc "A CLI tool to understand HTTP codes and cats."
     homepage "https://github.com/Schrodinger-Hat/cattp"
     url "https://github.com/Schrodinger-Hat/cattp/raw/main/cattp.tar.gz"
-    sha256 ""
+    sha256 "5901dfc48d3b3404cc4f372eb6cbeecab11195c4"
     version "1.0.1"
     license "MIT"
   
