6e60bdfb9555e01e4af03091037fd355263314b3
Lorenzo Pieri <lorenzo.pieri@birdie.care>
2023-06-22 00:02:53 +0200

    Release new version

--- Formula/cattp.rb
+++ Formula/cattp.rb
@@ -3,7 +3,7 @@ class Cattp < Formula
     homepage "https://github.com/Schrodinger-Hat/cattp"
     url "https://github.com/Schrodinger-Hat/cattp/raw/main/cattp.tar.gz"
     sha256 "fb759ae362bbfc4f41090cf4c97855226c67ff6a63369629e5ab417131387279"
-    version "1.0.3"
+    version "1.0.4"
     license "MIT"
   
     def install
